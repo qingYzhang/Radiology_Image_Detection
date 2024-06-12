4cb0625ba2cde48ca7d9a803afabb8410c947718
Zhang <zhangs18@bigpurple-ln3.cm.cluster>
2024-06-12 15:46:48 -0400

    update

--- nets/swin_transformer.py
+++ nets/swin_transformer.py
@@ -1,10 +1,3 @@
-# --------------------------------------------------------
-# Swin Transformer
-# Copyright (c) 2021 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ze Liu
-# --------------------------------------------------------
-
 from ast import Str
 import math
 import warnings
